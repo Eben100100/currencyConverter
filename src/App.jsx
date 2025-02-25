d07e82884047422dbd25fe9ae4c05b9b07b12ff3
eben <toluarowolo1914@gmail.com>
2025-02-25 19:10:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [amount, setAmount] = useState(1);
+  const [inputCur, setInputCur] = useState("USD");
+  const [outputCur, setOutputCur] = useState("EUR");
+  const [convertedCur, setConvertedCur] = useState("");
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${inputCur}&to=${outputCur} `
+        );
+        if (!res.ok) throw new Error("Can not fetch data");
+
+        const data = await res.json();
+        setConvertedCur(data.rates[outputCur]);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setError("");
+      }
+    };
+    fetchData();
+  }, [inputCur, outputCur, amount]);
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(+e.target.value)}
+      />
+      <select value={inputCur} onChange={(e) => setInputCur(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={outputCur} onChange={(e) => setOutputCur(e.target.value)}>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+        <option value="CAD">CAD</option>
+      </select>
+
+      {error && <p style={{ color: "red" }}> 💥💥 {error} 💥💥 </p>}
+      {!error && <p> {convertedCur} </p>}
+    </div>
+  );
+}
+
+export default App;
